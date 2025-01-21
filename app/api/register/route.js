1d435543fcb5a2eba8d88d79997b620cd6df5c84
kb603 <kojoaddobaffour@gmail.com>
2025-01-21 18:26:49 +0000

    bug fix: db sign up

---
+++ app/api/register/route.js
@@ -0,0 +1,25 @@
+import { connectMongoDB } from "../../../lib/mongodb";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "../../../models/user";
+
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
+
+    console.log(name, email, hashedPassword);
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while registering user" },
+      { status: 500 }
+    );
+  }
+}
